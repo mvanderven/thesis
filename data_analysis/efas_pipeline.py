55121d9113d9429d98f640bd85e5e25760fc6643
mvanderven <mizzivanderven@gmail.com>
2021-02-26 10:38:19 +0100

    Updated imported modules

--- data_analysis/efas_pipeline.py
+++ data_analysis/efas_pipeline.py
@@ -16,9 +16,9 @@ import matplotlib.pyplot as plt
 import cartopy 
 
 ## import own functions  --> check bar in top right hand to directory of files
-import dev_utils
-import dev_plotter 
-import dev_signatures 
+import thesis_utils as utils 
+import thesis_plotter as plotter 
+import thesis_signatures 
 
 #%% 
 
@@ -28,13 +28,13 @@ model_data = Path(r"C:\Users\mvand\Documents\Master EE\Year 4\Thesis\data\model_
 gauge_data = Path(r"C:\Users\mvand\Documents\Master EE\Year 4\Thesis\data\gauge_data") 
 
 ## model data paths 
-model_data_dict = dev_utils.get_file_paths(model_data, 'nc', long_name=True)
+model_data_dict = utils.get_file_paths(model_data, 'nc', long_name=True)
 keys = list(model_data_dict.keys())
 
 keys_efas = [key for key in keys if 'EFAS' in key]
 
 ## gauge data - GRDC
-gauge_data_dict = dev_utils.get_file_paths(gauge_data, 'txt', long_name=True)
+gauge_data_dict = utils.get_file_paths(gauge_data, 'txt', long_name=True)
 gauge_keys = list( gauge_data_dict.keys() )
 print('File names loaded \n')
 
@@ -42,7 +42,7 @@ print('File names loaded \n')
 
 print('Load EFAS data')
 efas_dir = model_data_dict[keys_efas[2]]
-ds_efas = dev_utils.open_efas(efas_dir)
+ds_efas = utils.open_efas(efas_dir)
 print('EFAS data loaded \n')
 
 #%%
@@ -50,14 +50,16 @@ print('EFAS data loaded \n')
 print('Load gauge data')
 ## load gauge data 
 gauge_file_names = gauge_data_dict[gauge_keys[0]] 
-gauge_data_grdc, meta_grdc = dev_utils.read_gauge_data( gauge_file_names, dtype='grdc')
+gauge_data_grdc, meta_grdc = utils.read_gauge_data( gauge_file_names, dtype='grdc')
+
+print(gauge_data_grdc)
  
 gauge_locs = gauge_data_grdc['loc_id'].unique()
 lat_coords = gauge_data_grdc['lat'].unique()
 lon_coords = gauge_data_grdc['lon'].unique() 
 
 coords_4326 = np.array([lon_coords, lat_coords]).transpose()
-coords_3035 = dev_utils.reproject_coordinates(coords_4326, 4326, 3035)
+coords_3035 = utils.reproject_coordinates(coords_4326, 4326, 3035)
 print('Gauge data loaded \n')
 
 
@@ -77,7 +79,7 @@ time_search = {
             'method': None
             }}
 
-efas_time = dev_utils.search_ds(ds_efas, time_search, return_df = False)
+efas_time = utils.search_ds(ds_efas, time_search, return_df = False)
 print('Time search done \n')
 
 #%% 
@@ -112,7 +114,7 @@ for i in range(len(gauge_locs)):
     loc= gauge_locs[i]
 
     ## efas buffer search
-    efas_buffer = dev_utils.iterative_pixel_search(efas_time,
+    efas_buffer = utils.iterative_pixel_search(efas_time,
                                                     loc,
                                                     init_x = coords_3035[i][0],
                                                     init_y = coords_3035[i][1],
@@ -144,32 +146,33 @@ efas_set = collect_efas[ collect_efas['match_gauge'].isin(subset_locations)]
 
 #%% 
 
-show_efas_search = dev_plotter.dashboard(efas_time, efas_set, gauge_set, subset_locations)
+### fix bug 
+# show_efas_search = plotter.dashboard(efas_time, efas_set, gauge_set, subset_locations)
  
 #%% 
 
 ### SIGNATURES 
 calc_features = [
-                    # 'normal', 
-                    #  'log', 
-                    #    'gev', 
-                    #  'gamma', 
-                    #  'n-acorr', 
-                    #  'n-ccorr',
-                    # 'fdc-q', 'fdc-slope', 'lf-ratio',
-                    # 'bf-index', 
-                    # 'dld', 
-                    # 'rld', 
-                    # 'rbf',
+                    'normal', 
+                      'log', 
+                        'gev', 
+                      'gamma', 
+                      'n-acorr', 
+                      'n-ccorr',
+                    'fdc-q', 'fdc-slope', 'lf-ratio',
+                    'bf-index', 
+                    'dld', 
+                    'rld', 
+                    'rbf',
                     'src'
                    ]
 
-efas_feature_table = dev_signatures.calc_features(gauge_time, collect_efas, gauge_locs, features=calc_features,
+efas_feature_table = thesis_signatures.calc_features(gauge_time, collect_efas, gauge_locs, features=calc_features,
                                                   n_lag=[1,2,5], n_cross = [0, 1, 5])
 
+print(efas_feature_table)
 
-
-
+# efas_feature_table.to_csv('test.csv')
 
 
 
