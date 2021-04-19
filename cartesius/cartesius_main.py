678147e8d4cd8bcbe10dd3405d9592edfb545e7c
mvanderven <mizzivanderven@gmail.com>
2021-04-19 08:13:54 +0200

    Functionalities for parallel processing

---
+++ cartesius/cartesius_main.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 16 13:52:31 2021
+
+@author: mvand
+"""
+
+#%% Import modules 
+import pandas as pd  
+from pathlib import Path 
+import time 
+import pathos as pa 
+
+from cartesius_utils import load_efas, buffer_search
+
+#%% Set file paths 
+
+fn = 'V1_grdc_efas_selection-cartesius.csv'
+efas_dir = Path(r"C:\Users\mvand\Documents\Master EE\Year 4\Thesis\data\model_data\EFAS_6h")
+# efas_dir = Path(r"C:\Users\mvand\Documents\Master EE\Year 4\Thesis\data\model_data\EFAS_6h_test")
+
+
+#%% Define function 
+
+def run_parallel(fn, efas_dir):
+    
+    ## load data 
+    df = pd.read_csv(fn, index_col = 0) 
+    ds = load_efas(efas_dir) 
+    
+    ## set function aprameters 
+    test_ix = df.index.values
+    df_list = [df]*len(test_ix) 
+    ds_list = [ds]*len(test_ix) 
+    
+    print('Start parallel processing')
+    time_parallel = time.time() 
+    
+    ## create pool
+    p = pa.pools.ParallelPool(nodes=4) 
+    
+    ## map 
+    results_parallel = p.map(buffer_search, test_ix, df_list, ds_list) 
+    
+    print('{:.2f} minutes'.format( (time.time() - time_parallel)/60. )) 
+    
+    ## collect data
+    ## reorder results_parallel
+    df_collect = pd.DataFrame() 
+    for df_result in results_parallel:
+        df_collect = df_collect.append(df_result) 
+    return df_collect 
+
+
+#%% Run 
+
+
+if __name__ == '__main__':
+    
+    df = run_parallel(fn, efas_dir)
+    
+    print(df)
+    print('########') 
+    print(df.info())
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ cartesius/cartesius_utils.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 16 16:21:29 2021
+
+@author: mvand
+"""
+
+    
+import pandas as pd  
+import xarray as xr     
+
+def load_efas(efas_dir, do_resample_24hr = False): 
+
+    ## get file names in dir 
+    files = [file for file in efas_dir.glob('*.nc')] 
+    
+    ## load list 
+    ds_out = xr.open_mfdataset(files, chunks = {"time": 10}) 
+    
+    ## resample from 6 hourly data to 24 hourly data 
+    if do_resample_24hr:
+        ds_out = ds_out.resample(time="1D").mean() 
+        
+        ds_out = ds_out.rename( {'dis06': 'dis24'})
+    
+    ## rename variables for consistency
+    ds_out = ds_out.rename(name_dict={  'latitude': 'lat',
+                                        'longitude': 'lon'})
+    
+    return ds_out 
+
+
+def buffer_search(gauge_id, df, ds, buffer_size = 2, cell_size = 5000., var = 'dis06'): 
+        
+    ## create empty output dataframe
+    out_df = pd.DataFrame()
+        
+    gauge_X, gauge_Y = df.loc[gauge_id, ['proj_X', 'proj_Y']]
+    
+    ## find closest cell center form (gauge_X, gauge_Y)
+    center_search = ds.sel( {'x': gauge_X,
+                             'y': gauge_Y},
+                           method = 'nearest') 
+    
+    ## extract x and y from search results 
+    center_cell_X = center_search.x.values 
+    center_cell_Y = center_search.y.values 
+    
+    ## create a searching area 
+    ## based on center_cell XY and buffer size 
+    mask_x = ( ds['x'] >=  center_cell_X - (1.1*buffer_size*cell_size) ) & ( ds['x'] <=  center_cell_X + (1.1*buffer_size*cell_size) )
+    mask_y = ( ds['y'] >= center_cell_Y - (1.1*buffer_size*cell_size) ) & ( ds['y'] <= center_cell_Y + (1.1*buffer_size*cell_size) ) 
+    
+    ## execute mask search 
+    ds_buffer = ds.where( mask_x & mask_y, drop=True)
+    
+    ## get coordinates 
+    buffer_x = ds_buffer['x'].values 
+    buffer_y = ds_buffer['y'].values 
+    
+    ## convert xarray to dataframe 
+    df_buffer = ds_buffer.to_dataframe() 
+
+    for i in range(len(buffer_x)):
+        for j in range(len(buffer_y)):
+            
+            ## get specific cell from df_buffer
+            df_cell = df_buffer.loc[:, buffer_x[i], buffer_y[j]][[var, 'lat', 'lon', 'upArea']]
+            df_cell = df_cell.reset_index() 
+            
+            ## add metadata 
+            df_cell['gauge'] = gauge_id 
+            df_cell['ID'] = 'cell_{}_{}{}'.format(gauge_id, i,j)
+            df_cell = df_cell.set_index('ID')
+            
+            ## append to output dataframe 
+            out_df = out_df.append(df_cell)
+            
+    return out_df 
